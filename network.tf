6a0273555eaad4d96ec716df299b9c1e3ae1af23
Sarath Pillai <sarathpillai@Saraths-MacBook-Air.local>
2022-06-07 17:30:26 +0530

    fixing ax

--- network.tf
+++ network.tf
@@ -3,7 +3,7 @@ module "vpc" {
   name = "rc-demo"
   cidr = "10.1.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
   enable_nat_gateway = true
