48f83985c8403903b34d3a735ebcadd19afbc3e0
Sarath Pillai <sarathpillai@Saraths-MacBook-Air.local>
2022-06-07 20:22:20 +0530

    fixing data expansion

--- dns.tf
+++ dns.tf
@@ -2,12 +2,12 @@ resource "aws_route53_zone" "eks" {
   name = var.domain_name
 }
 
-resource "aws_route53_record" "www-dev" {
+resource "aws_route53_record" "wildcard" {
   zone_id = aws_route53_zone.eks.zone_id
   name    = "*"
   type    = "CNAME"
   ttl     = "300"
-  records = tolist(data.kubernetes_service.nginx.spec[0].external_ips)
+  records = [data.kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname]
   depends_on = [
     helm_release.ingress-nginx
   ]
