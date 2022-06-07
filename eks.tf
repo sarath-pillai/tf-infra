44af0266e8d44c884b82b6786b3f9984430bc5a2
Sarath Pillai <sarathpillai@Saraths-MacBook-Air.local>
2022-06-07 17:56:23 +0530

    avoid causing changes for kubeconfig

--- eks.tf
+++ eks.tf
@@ -11,6 +11,11 @@ module "cluster" {
   enable_irsa                    = false
   manage_aws_auth                = false
   default_platform               = "linux"
+  # write_kubeconfig cause permanent change detection by terraform
+  # and is not a good method
+  # See https://github.com/terraform-aws-modules/terraform-aws-eks/issues/1012#issuecomment-695028105
+  # for a better solution to get the kube config info
+  write_kubeconfig               = false
   cluster_endpoint_public_access = true
   worker_ami_owner_id            = "296578399912"
   node_groups                    = var.clusters
