b89d4a40840f7a7b7f5643cae71183a9fa8b07bf
Sarath Pillai <sarathpillai@Saraths-MacBook-Air.local>
2022-06-07 17:54:38 +0530

    config map and fmt

--- eks.tf
+++ eks.tf
@@ -9,8 +9,7 @@ module "cluster" {
   )
   vpc_id                         = module.vpc.vpc_id
   enable_irsa                    = false
-  create_aws_auth_configmap      = false
-  manage_aws_auth_configmap      = false
+  manage_aws_auth                = false
   default_platform               = "linux"
   cluster_endpoint_public_access = true
   worker_ami_owner_id            = "296578399912"
